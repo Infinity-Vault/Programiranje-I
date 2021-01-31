c3ea1603db830f2ac1899bc812b6e4a544fddd78
AdivonSlav <efendic100@gmail.com>
2021-01-31 14:26:06 +0100

    Dodao prvi i drugi zadatak sa ispita

---
+++ Zadatak 1/01.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float proracun(float x, char izbor);
+
+int main()
+{
+	float x;
+	char izbor;
+
+	cout << "Unesite realni broj x: ";
+	cin >> x;
+
+	do
+	{
+		cout << "Unesite zeljenu funkciju (s - sin, c - cos): ";
+		cin >> izbor;
+
+		if (izbor != 's' && izbor != 'c')
+			cout << "Niste unijeli korektnu funkciju, ponovite unos" << endl;
+		
+	} while (izbor != 's' && izbor != 'c');
+
+	cout << "Proracun datog izraza je: " << proracun(x, izbor) << endl;
+
+	return 0;
+}
+
+float proracun(float x, char izbor)
+{
+	if (izbor == 's')
+	{
+		float rezultat = 0.0f;
+
+		for (int i = 1; i <= x; i++)
+		{
+			rezultat += i / (sin(x / i) + x);
+		}
+
+		return rezultat;
+	}
+
+	else
+	{
+		float rezultat = 0.0f;
+
+		for (int i = 1; i <= x; i++)
+		{
+			rezultat += i / (cos(x / i) + x);
+		}
+
+		return rezultat;
+	}
+}
---
+++ Zadatak 2/01.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int generisiBacanja();
+
+int main()
+{
+	srand(time(NULL));
+
+	cout << "Tri ista broja dobila su se dva puta uzastopno nakon " << generisiBacanja() << " bacanja" << endl;
+	
+	return 0;
+}
+
+int generisiBacanja()
+{
+	int broj_bacanja = 0;
+	int broj_istih = 0;
+
+	do
+	{
+		int kocka1 = rand() % 6 + 1;
+		int kocka2 = rand() % 6 + 1;
+		int kocka3 = rand() % 6 + 1;
+
+		if (kocka1 == kocka2 == kocka3)
+		{
+			broj_istih++;
+		}
+
+		else
+			broj_istih = 0;
+
+		broj_bacanja++;
+		
+	} while (broj_istih < 2);
+
+	return broj_bacanja;
+}
