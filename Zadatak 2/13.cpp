6ddfcf8a114f8694b72fb294694497dba83113bc
pointer2pointer <78883395+pointer2pointer@users.noreply.github.com>
2021-02-10 23:32:17 +0100

    Update 13.cpp
    -

--- Zadatak 2/13.cpp
+++ Zadatak 2/13.cpp
@@ -8,7 +8,7 @@ int main() {
 		godisnjirastC = 4.30;
 	double firmaA = 10, firmaB = 7, firmaC = 5;
 	int brojGodina = 0;
-	while (firmaB < firmaA || firmaC < firmaA) {
+	while (firmaB <= firmaA && firmaC <= firmaA) {
 		firmaA = firmaA + (firmaA * godisnjirastA / 100.0);
 		firmaB = firmaB + (firmaB * godisnirastB / 100.0);
 		firmaC = firmaC + (firmaC * godisnjirastC / 100.0);
@@ -19,11 +19,9 @@ int main() {
 	else
 		cout << "Firma C je prva prestigla firmu A za " << brojGodina << " godina." << endl;
 
-	cout << "Godisnji prihod firme A u " << brojGodina << " godini iznosi " << setprecision(2) << firmaA << " M EURA." << endl;
-	cout << "Godisnji prihod firme B u " << brojGodina << " godini iznosi " << setprecision(2) << firmaB << " M EURA." << endl;
-	cout << "Godisnji prihod firme C u " << brojGodina << " godini iznosi " << setprecision(2) << firmaC << " M EURA." << endl;
-
-
+	cout << "Godisnji prihod firme A u " << brojGodina << " godini iznosi " << fixed << setprecision(2) << firmaA << " M EURA." << endl;
+	cout << "Godisnji prihod firme B u " << brojGodina << " godini iznosi " << firmaB << " M EURA." << endl;
+	cout << "Godisnji prihod firme C u " << brojGodina << " godini iznosi " << firmaC << " M EURA." << endl;
 
 	cin.get();
 	return 0;
