7102dbab6ac4704fa79a687f089aa201fb8cf64d
BerunBiH <76669701+BerunBiH@users.noreply.github.com>
2021-02-17 00:41:17 +0100

    Update 11.cpp

--- Zadatak 4/11.cpp
+++ Zadatak 4/11.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include<time.h>
+#include<time.h> //dodajemo kako bi komanda na 11 liniji radila
 using namespace std;
-const int x = 10, y = 10;
+const int x = 10, y = 10; //deklarišemo dvije konstante za našu matricu gdje je x red a y kolona
 
-int brCif(int);
-void unos(int[][y]);
-void ispis(int[][y]);
-void red_i_kolona(int[][y]);
+int brCif(int); //funkcija koja racuna koliko cifri ima nas uneseni broj
+void unos(int[][y]); //funckija za unos elemenata u matricu
+void ispis(int[][y]); //funkcija za ispis matrice
+void red_i_kolona(int[][y]); //funkcija za racunanje najboljeg reda i kolone
 int main() {
-	srand((time(0)));
+	srand((time(0))); //sluzi za generisanje uvijek drugacijek slucajnog broja pri pokretanju programa, ako bi se izbrisala ova linija slucajni broj kod plate uvijek bi bio isti
 	int niz[x][y];
 	unos(niz);
 	ispis(niz);
@@ -33,13 +33,14 @@ void unos(int niz[][y]) {
 	{
 		for (int j = 0; j < y; j++)
 		{
-			do {
+			do { 
 				//cin >> temp;
 				temp = rand() % 1000 + 1;
 				niz[i][j] = temp;
-				if (brCif(temp) % 2 == 0)
+				/*if (brCif(temp) % 2 == 0)
 					cout << "Unijeli ste broj sa PARNIM  brojem cifri!!!" << endl;
-			} while (brCif(temp) % 2 == 0);
+					*/ //posto unosimo sa komandom rand a ne manuelno ovaj if nam ne teba a i kod manuelnog unosenja stavljamo ga samo da ljepse kod izgleda
+			} while (brCif(temp) % 2 == 0); //da se ponavlja unos sve dok je broj cifri paran
 
 		}
 	}
@@ -57,24 +58,24 @@ void ispis(int niz[][y]) {
 	}
 }
 void red_i_kolona(int niz[][y]) {
-	float rednP[x] = { 0 };
-	float kolonanP[y] = { 0 };
+	float rednP[x] = { 0 }; //pomocni niz u kojeg spremamo sve vrijednosti redova
+	float kolonanP[y] = { 0 }; //pomocni niz za kolone gdje spremamo sve vrijednosti kolona
 	for (int i = 0; i < x; i++)
 	{
 		for (int j = 0; j < y; j++)
 		{
-			rednP[i] += niz[i][j];
+			rednP[i] += niz[i][j]; //sabiramo sve elemente jednog reda u jednu jedinstvenu cifru
 		}
-		rednP[i] /= y;
+		rednP[i] /= y; //djelimo sa brojem kolona tako da dobijemo prosjek
 	}
 	int indeksNajvecegR = 0;
 	for (int i = 0; i < x; i++)
 	{
-		if (rednP[i] > rednP[indeksNajvecegR]) {
+		if (rednP[i] > rednP[indeksNajvecegR]) { //radimo poredenje razlicitih elemenata u pomocnom nizu tako da saznamo koji je red najveci
 			indeksNajvecegR = i;
 		}
 	}
-	//cout << "Indeks reda sa najvecim prosjekom je: " << indeksNajvecegR << endl;
+	cout << "Indeks reda sa najvecim prosjekom je: " << indeksNajvecegR << endl;
 	cout << "\n";
 	cout << "Ispis reda sa najvecim prosjekom: " << endl;
 	for (int j = 0; j < y; j++)
@@ -87,18 +88,18 @@ void red_i_kolona(int niz[][y]) {
 	{
 		for (int j = 0; j < y; j++)
 		{
-			kolonanP[i] += niz[j][i];
+			kolonanP[i] += niz[j][i]; //sabiramo sve elemente jedne kolone u jedinstvenu cifru
 		}
-		kolonanP[i] /= x;
-	}
+		kolonanP[i] /= x; //djelimo sa brojem redova da dobijemo prosjek te kolone
+	} 
 	int indeksNajveceK = 0;
 	for (int i = 0; i < y; i++)
 	{
-		if (kolonanP[i] > kolonanP[indeksNajveceK]) {
+		if (kolonanP[i] > kolonanP[indeksNajveceK]) { //radimo poredenje razlicitih elemenata u pomocnom nizu tako da saznamo koja je kolona najveci
 			indeksNajveceK = i;
 		}
 	}
-	//cout << "Indeks kolone sa najvecim prosjekom je: " << indeksNajveceK << endl;
+	cout << "Indeks kolone sa najvecim prosjekom je: " << indeksNajveceK << endl;
 	cout << "\n";
 	cout << "Ispis kolone sa najvecim prosjekom: " << endl;
 	for (int i = 0; i < x; i++)
