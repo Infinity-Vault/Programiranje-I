74a774e79cdbaecaa9e6169837ac6224cd9a55f0
BerunBiH <76669701+BerunBiH@users.noreply.github.com>
2021-02-16 19:05:00 +0100

    Update 09.cpp

--- Zadatak 4/09.cpp
+++ Zadatak 4/09.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <time.h>
+#include <time.h> //ovo dodajemo da bi komanda na 34. liniji radila
 using namespace std;
 
 //Deklarisanje strukture:
@@ -26,12 +26,12 @@ struct uposlenik {
 
 };
 //Dekalracije funckija:
-void unos(uposlenik**, int, int);
-void ispis(uposlenik**, int, int);
-void odjel_sa_Najvecim_prosjekom(uposlenik**, int, int);
-void dealokacijaNiza(uposlenik**&, int, int);
+void unos(uposlenik**, int, int); //funkcija za unos elemenata niza 
+void ispis(uposlenik**, int, int); //funkcija za ispis elemenata niza
+void odjel_sa_Najvecim_prosjekom(uposlenik**, int, int); //funkcija koja sluzi za racunanje koji odjel ima najveci prosjek plate
+void dealokacijaNiza(uposlenik**&, int, int); //funkcija koja sluzi za dealokaciju niza
 int main() {
-    srand((time(0)));
+    srand((time(0))); //sluzi za generisanje uvijek drugacijek slucajnog broja pri pokretanju programa, ako bi se izbrisala ova linija slucajni broj kod plate uvijek bi bio isti
     int red, kolona;
     cout << "Unesite broj redova: " << endl;
     cin >> red;
@@ -67,6 +67,7 @@ void unos(uposlenik** niz, int red, int kolona) {
             cin.getline((*(niz + i) + j)->imePrezime, 20);
             /*cout << "Unesite platu uposlenika: " << endl;
             cin >> *(*(niz + i) + j)->plata;*/
+            //platu smo unjeli automatski sa kodom koji je dole prikazan dok zakomentirani gore kod sluzi za unos iznosa plate rucno tj. preko tastature
             *(*(niz + i) + j)->plata = rand() % 3000 + 1500;
         }
     }
@@ -82,11 +83,13 @@ void ispis(uposlenik** niz, int red, int kolona) {
             cout << "Ime i Prezime uposlenika je: " << (*(niz + i) + j)->imePrezime << "\t";
             cout << "Plata uposlenika je: " << *(*(niz + i) + j)->plata << " KM" << endl;
         }
+        //stavili smo ovaj endl ovdje da vidimo odakle pocinje novi red u nasoj matrici
         cout << endl;
     }
 }
 void odjel_sa_Najvecim_prosjekom(uposlenik** niz, int red, int kolona) {
-    double* prosjekOdjela = new double[red] {};
+    double* prosjekOdjela = new double[red] {}; //pravimo pomocni niz koji ima velicinu broja nasih redova u taj pomocni niz spremamo prosjecnu platu svakog reda
+    // {} nakon deklarisanja niza sluzi da sve elemente niza postavimo na 0 sto nam je vazno jer cemo dole vrsiti sabiranje
     for (int i = 0; i < red; i++)
     {
         for (int j = 0; j < kolona; j++)
@@ -96,24 +99,26 @@ void odjel_sa_Najvecim_prosjekom(uposlenik** niz, int red, int kolona) {
         *(prosjekOdjela + i) /= kolona;//Dijelim sa onoliko plata koliko sam i sabrao tj sa kolonom, i dobijam prosjecnu vrijednost svakog reda;
     }
     int najbolji_prosjek_Indeks = 0;
-    for (int i = 0; i < red; i++)
+    for (int i = 0; i < red; i++) 
     {
-        if (*(prosjekOdjela + i) > * (prosjekOdjela + najbolji_prosjek_Indeks)) {
+        if (*(prosjekOdjela + i) > *(prosjekOdjela + najbolji_prosjek_Indeks)) //oov je najobicnije poredenje gdje gledamo koji element naseg pomocnog niza je najveci 
+        {
             najbolji_prosjek_Indeks = i;
         }
     }
     cout << "Indeks odjela (reda) sa najvecim prosjekom je: " << najbolji_prosjek_Indeks << " a najbolji prosjek je: " << *(prosjekOdjela + najbolji_prosjek_Indeks) << endl;
-    delete[]prosjekOdjela;
+    delete[]prosjekOdjela; //ne smijemo zaboraviti izbrisati nas pomocni niz jer i on je dinamički
     prosjekOdjela = nullptr;
 
 }
 
 void dealokacijaNiza(uposlenik**& niz, int red, int kolona) {
+    //ne moramo zauprave kolone ovdje ni prosljediti jer ih necemo koristiti
     for (int i = 0; i < red; i++)
     {
-        delete[] * (niz + i);
+        delete[] * (niz + i); //ovdje brisemo nase kolone u nizu
         *(niz + i) = nullptr;
     }
-    delete[]niz;
-    niz = nullptr;
-}
+    delete[]niz; //ovdje brisemo redove i sa tim postaje prazan niz
+    niz = nullptr; //dodjeljivamo da niz ne pokaziva ni na šta
+} 
