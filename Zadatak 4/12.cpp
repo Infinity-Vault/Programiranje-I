e299d67cc8a567094efae2c12f0ee32c2e88507b
BerunBiH <76669701+BerunBiH@users.noreply.github.com>
2021-02-17 00:57:09 +0100

    Update 12.cpp

--- Zadatak 4/12.cpp
+++ Zadatak 4/12.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
-#include <iomanip>
-#include <time.h>
+#include <iomanip> 
+#include <time.h> //dodajemo da bi radila komanda na 14. liniji
 using namespace std;
 
-int brojCifri(int);
-bool Crno_polje(int, int);
+int brojCifri(int); //funkcija koja racuna koliko cifri nas broj ima
+bool Crno_polje(int, int); //funkcija koja nam govori koje je crno polje u matrici
 
-void dealokacija_niza(int**&, int, int);
-void unos_Niza(int**, int, int);
-void ispis_Niza(int**, int, int);
-void prosjeci_bijelih_crnih(int**, int, int);
+void dealokacija_niza(int**&, int, int); //funkcija koja brise dinamicki dvodimenzionalni niz
+void unos_Niza(int**, int, int); //funkcija koja sluzi za unos elemenata u dinamicki dvodimenzionalni niz
+void ispis_Niza(int**, int, int); //funkcija koja sluzi za ispis elemenata niza
+void prosjeci_bijelih_crnih(int**, int, int); //funkcija koja racuna prosjek svih elemenata na bijelim i na crnim poljima
 int main() {
-	srand((time(0)));
+	srand((time(0))); //sluzi za generisanje uvijek drugacijek slucajnog broja pri pokretanju programa, ako bi se izbrisala ova linija slucajni broj kod plate uvijek bi bio isti
 	int red, kolona;
 	cout << "Molim vas uneiste koliko redova zelite: " << endl;
 	cin >> red;
 	cout << "Unesite koliko kolona zelite: " << endl;
 	cin >> kolona;
+	//kreiramo sada nas dvodimenzionalni dinamicki niz
 	int** niz = new int* [red];
 	for (int i = 0; i < red; i++)
 	{
@@ -41,11 +42,11 @@ int main() {
 void dealokacija_niza(int**& niz, int red, int kolona) {
 	for (int i = 0; i < red; i++)
 	{
-		delete[] * (niz + i);
+		delete[] * (niz + i); //sa ovim brisemo kolone dvodimenzionalnog dinamickog niza
 		*(niz + i) = nullptr;
 	}
-	delete[]niz;
-	niz = nullptr;
+	delete[]niz; //sa ovim brisemo redove dinamickog dvodimenzionalnog niza i u biti stvaramo prazan pointer sto pokaziva na nista
+	niz = nullptr; //sa ovim stavljamo da pointer pokaziva na 0
 }
 int brojCifri(int broj) {
 	int br = 0;
@@ -56,7 +57,7 @@ int brojCifri(int broj) {
 	return br;
 }
 bool Crno_polje(int i, int j) {
-	if ((i + j) % 2 == 0)
+	if ((i + j) % 2 == 0) //uzeli smo da ako nam je zbroj reda i kolone djeljiv sa 2 da nam je to crno polje
 		return true;
 	else
 		return false;
@@ -73,6 +74,7 @@ void unos_Niza(int** niz, int red, int kolona) {
 				do {
 					cout << "Unesite paran broj sa neparnim brojem cifri: " << endl;
 					cin >> temp;
+					//sa ovim rand automatiziramo unos a sa ovim vamo dole mi rucno unosimo elemente
 					//temp = rand() % 1000 + 100;
 					*(*(niz + i) + j) = temp;
 					if (temp % 2 != 0)
@@ -126,6 +128,27 @@ void prosjeci_bijelih_crnih(int** niz, int red, int kolona) {
 			}
 		}
 	}
+	/*
+	glavna dijagonala ide : 
+
+	 \	*	*	*	*
+	 *	\	*	*	*
+	 *	*	\	*	*
+	 *  *	*	\	*
+	 *	*	*	*	\
+	 
+
+	sporedna dijagonala ide : 
+
+	*	*	*	*	/
+	*	*	*	/	*
+	*	*	/	*	*
+	*	/	*	*	*
+	/	*	*	*	*
+
+
+
+	*/
 	cout << "Prosjek svih elemenata na crnim poljima ispod sporedne dijagonale je: " << setprecision(4) << prosjek_Crnih / float(brojacC) << endl;
 	cout << "Prosjek svih elemenata na bijelim poljima iznad glavne dijagonale je: " << setprecision(4) << prosjek_Bijelih / float(brojacB) << endl;
 }
