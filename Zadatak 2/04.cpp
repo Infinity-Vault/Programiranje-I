e5ba558b22809583710852f83f94ec07ecf6f44e
Faris Skopak <77499895+R3FA@users.noreply.github.com>
2021-02-17 16:32:42 +0100

    Update 04.cpp

--- Zadatak 2/04.cpp
+++ Zadatak 2/04.cpp
@@ -1,40 +1,63 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-bool isProst(int broj)
+/*
+Svaki paran broj se može prikazati kao suma dva prosta broja (tkz. Goldbachovo pravilo). Napisati program koji će najprije učitati
+dva prirodna broja n1 i n2 (n2 > n1). Zatim prikazati sve parne brojeve u intervalu od n1 do n2 kao sumu dva prosta broja.
+U glavnom programu samo unijeti navedena dva prirodna broja i pozvati funkciju koja obavlja zadani posao.
+*/
+
+bool Prost(int);
+void Goldbach(int);
+void PozivGoldbacha(int, int);
+
+int main()
 {
-	for (int i = 2; i < broj; i++)		// Obicna funkcija za provjeravanje prostog broja.
+	int n1, n2;
+	do
+	{
+		cout << "Unesite prvi prirodan broj: " << endl;
+		cin >> n1;
+		cout << "Unesite drugi prirodni broj: " << endl;
+		cin >> n2;
+	
+	} while (n1 <= 0 || n2 <= 0);
+	cout << endl;
+
+	PozivGoldbacha(n1, n2);
+
+	system("pause>0");
+	return 0;
+}
+
+bool Prost(int broj)
+{
+	for (int i = 2; i < broj; i++)
 		if (broj % i == 0)
 			return false;
-	return true;						
+	return true;
 }
 
 void Goldbach(int broj)
 {
-	for (int i=broj/2, j=broj/2; i<=broj; i++, j--)
-		if (isProst(i) && isProst(j))				// Brojaci krecu sa polovine broja, prvi se povecava a drugi smanjuje.
+	for (int i = broj / 2, j = broj / 2; i <= broj; i++, j--)
+		if (Prost(i) && Prost(j))
 		{
 			cout << broj << "=" << i << "+" << j << endl;
-			return;				// Return koristimo kao prekid funkcije jer nam je dovoljan samo jedan dokaz, a u isto vrijeme i ubrzava izvrsenje koda.
+			break;
 		}
 }
 
-
-int main() 
+void PozivGoldbacha(int n1, int n2)
 {
-	int n1, n2;
-	do {
-		cout << "Unesite n1: ";
-		cin >> n1;
-		cout << "Unesite n2: ";
-		cin >> n2;
-	} while (n1 == n2 || n1>n2);					// n1 i n2 ne smiju biti jednaki, niti n1 smije biti veci od n2.
-
+	if (n1 > n2)
+	{
+		int temp = n1;
+		n1 = n2;
+		n2 = temp;
+	}
 	if (n1 % 2 != 0)
-		n1++;			// Zbog iduce for petlje trebamo poceti sa parnim brojem, inace ce petlja ici kroz neparne brojeve, u slucaju da je n1 neparan, u ovoj liniji koda se n1 povecava za 1, sto ga cini parnim.
-	
-	for (int i = n1; i <= n2; i += 2)			// For petlja prolazi kroz sve parne brojeve u intervalu i salje ih u funkciju Goldbach.
+		n1++;
+	for (int i = n1; i <= n2; i += 2)
 		Goldbach(i);
-	
-	return 0;
 }
