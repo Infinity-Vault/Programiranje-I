ed0524ea85052f9eea8930b96c85b95c9b100ff2
Sanjin Pajic <68828360+ZCortex@users.noreply.github.com>
2021-02-17 01:35:08 +0100

    Update 05.cpp
    Referenca N u funkciji nije primala nikakvu vrijednost

--- Zadatak 2/05.cpp
+++ Zadatak 2/05.cpp
@@ -1,18 +1,6 @@
-void promjena(int&);
-int main() {
-	int N;
-	do {
-		cout << "Molim vas unesite broj: " << endl;
-		cin >> N;
-	} while (N > 5000000 || N < 50);
-	promjena(N);
+#include <iostream>
+using namespace std;
 
-
-
-
-	cin.get();
-	return 0;
-}
 void promjena(int& N) {
 	int pomocna = 0;
 	int ostatak = 0;
@@ -26,5 +14,20 @@ void promjena(int& N) {
 			N /= 10;
 		}
 	}
-	cout << "Uneseni broj procitan naopako bez neparnih cifri je: " << pomocna << endl;
+	N = pomocna;
+	cout << "Uneseni broj procitan naopako bez neparnih cifri je: " << N << endl;
 }
+
+int main() {
+	int N;
+	do {
+		cout << "Molim vas unesite broj: " << endl;
+		cin >> N;
+	} while (N > 5000000 || N < 50);
+	
+	promjena(N);
+
+	cin.get();
+	return 0;
+}
+
