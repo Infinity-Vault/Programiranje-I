89e8730875f909f74fca409697d10646bd10f00b
BerunBiH <76669701+BerunBiH@users.noreply.github.com>
2021-02-17 01:22:53 +0100

    Update 14.cpp

--- Zadatak 4/14.cpp
+++ Zadatak 4/14.cpp
@@ -9,7 +9,7 @@ struct student
     void ispis();
     void dealokacija();
 };
-void student::unos()
+void student::unos() //funkcija koju cemo pozvati za unos obiljezija objekata
 {
     cin.ignore();
     ID = new char[10];
@@ -22,14 +22,14 @@ void student::unos()
     cout << "Unesite prosjek : " << endl;
     cin >> *prosjek;
 }
-void student::ispis()
+void student::ispis() //funkciju koju cemo pozivati za ispis obiljezija
 {
     cout << endl;
     cout << "Vas ID je : " << ID << endl;
     cout << "Vase ime i prezime je : " << imePrezime << endl;
     cout << "Vas prosjek je : " << *prosjek << endl;
 }
-void student::dealokacija()
+void student::dealokacija() //funkciju koju cemo pozivati za dealokaciju obiljezija objekata
 {
     delete[] ID;
     ID = NULL;
@@ -38,11 +38,11 @@ void student::dealokacija()
     delete prosjek;
     prosjek = NULL;
 }
-void Unos(student**, int);// nema jos jedan int jer je isti broj redova i kolona pa imamo univerzalnu varijablu za to
-void Ispis(student**, int);
-void NajboljiFax(student**, int);
-void NajboljiStudent(student**, int);
-void DEALOKACIJA(student**&, int);
+void Unos(student**, int);// nema jos jedan int jer je isti broj redova i kolona pa imamo univerzalnu varijablu za to a funkcija sluzi za unos elemenata u dinamicki niz
+void Ispis(student**, int);// funkcija sluzi za ispis elemenata iz dinamickog niza
+void NajboljiFax(student**, int); //funkcija sluzi za pronalazenje fakulteta (reda) s najboljim prosjekom
+void NajboljiStudent(student**, int); //funkcija sluzi za pronalazenje studenta sa najboljim prsjekom u cjeloj matrici
+void DEALOKACIJA(student**&, int); //funkcija sluzi za dealokaciju elemenata dinamickog niza
 int main()
 {
     int univerzalno;
@@ -57,8 +57,10 @@ int main()
     {
         *(niz + i) = new student[univerzalno];
     }
+    //ispis funkcija
     Unos(niz, univerzalno);
     Ispis(niz, univerzalno);
+    cout << endl;
     NajboljiFax(niz, univerzalno);
     NajboljiStudent(niz, univerzalno);
     DEALOKACIJA(niz, univerzalno);
@@ -80,10 +82,10 @@ void Ispis(student** niz, int univerzalno)
 {
     for (int i = 0; i < univerzalno; i++)
     {
-        cout << "RED : " << i << endl;
+        cout << "RED : " << i << endl; //dodao sam ovo da se zna koji je tacno red 
         for (int j = 0; j < univerzalno; j++)
         {
-            cout << "KOLONA : " << j << endl;
+            cout << "KOLONA : " << j << endl; //da se zna tacno koja je tacno kolona u pitanju
             (*(*(niz + i) + j)).ispis();
         }
     }
@@ -103,13 +105,13 @@ void DEALOKACIJA(student**& niz, int univerzalno)
         *(niz + i) = NULL;
     }
     delete[]niz; //brise sve redove
-    niz = nullptr;
+    niz = nullptr; //stavljamo da pointer pokaziva na 0
 }
 void NajboljiFax(student** niz, int univerzalno)
 {
     //trazi se najbolji fakultet tj. najbolji red
     //sto podrazumjeva naci red s najboljim prosjekom
-    float* pomocni = new float[univerzalno]{};//ima velicinu nasih redova jer redove pretrazivamo
+    float* pomocni = new float[univerzalno] {};//ima velicinu nasih redova jer redove pretrazivamo
     //{} dodajemo jer to nam automatski sve elemente niza stavlja kao da su 0
     for (int i = 0; i < univerzalno; i++)
     {
@@ -125,13 +127,13 @@ void NajboljiFax(student** niz, int univerzalno)
         // npr.
         // pomocni [2,55,7,4]
         //indeks   [0, 1,2,3]
-        if (*(pomocni + najveci) < *(pomocni + i))
+        if (*(pomocni + najveci) < *(pomocni + i)) //radimo obicno poredenje elemenata pomocnog niza
         {
             najveci = i;
         }
     }
     cout << "Fakultet(red) s najvecim prosjekom je fakultet s indeksom : " << najveci << endl;
-    delete[]pomocni;
+    delete[]pomocni; //pomocni niz je dinamicki pa ga moramo onda i obrisati 
     pomocni = NULL;
 }
 void NajboljiStudent(student** niz, int univerzalna)
@@ -141,8 +143,8 @@ void NajboljiStudent(student** niz, int univerzalna)
     {
         for (int j = 0; j < univerzalna; j++)
         {
-            if((*(*(niz+najboljiRed)+najboljaKolona)->prosjek)<(*(*(niz+i)+j)->prosjek))
-            { 
+            if ((*(*(niz + najboljiRed) + najboljaKolona)->prosjek) < (*(*(niz + i) + j)->prosjek)) //opet radimo obicno poredenje ali sada poredimo sve elemente naseg dinamickog niza
+            {
                 najboljiRed = i;
                 najboljaKolona = j;
             }
