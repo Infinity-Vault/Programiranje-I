efaf33e78ee90433a0b744e5128bb5acee52e214
Haris Kordic <kordicharis18@gmail.com>
2021-05-18 12:03:12 +0200

    Update popravka logike

--- Zadatak 1/08.cpp
+++ Zadatak 1/08.cpp
@@ -1,54 +1,60 @@
-int unos();
-int Obrnibroj(int);
-int sumaCifri(int);
-void ispisXY(int, int);
-int main() {
+#include <iostream>
+using namespace std;
 
-	int x = unos();
-	int y = unos();
-	ispisXY(x, y);
+/*Omoguciti unos dva broja X i Y iz intervala od 10 do 5000. Kreirati program koji ce pronaci i ispisati sve brojeve od X do Y
+(ukljucujuci granicne vrijednosti) za koje važi da je obrnuti broj djeljiv sa njegovom sumom cifara.
+Za olakšanje rješenja zadataka napraviti funkcije za obrtanje poretka cifara i za sumu cifara.*/
 
-
-	cin.get();
-	return 0;
-
-}
-int unos() {
-	int broj;
-	do {
-		cout << "Unesite broj: " << endl;
+int unos()
+{
+    int broj;
+	do
+	{
+		cout << "Unesite broj od 10 do 5000 " << endl;
 		cin >> broj;
-	} while (broj < 10 || broj>5000);
+	} while (broj < 10 || broj >5000);
 	return broj;
 }
-int Obrnibroj(int broj) {
-	int obrnuti = 0;
-	while (broj > 0) {
-		obrnuti = obrnuti * 10 + broj % 10;
-		broj /= 10;
-	}
-	return obrnuti;
-}
-int sumaCifri(int broj) {
-	int Suma = 0;
-	while (broj > 0) {
-		Suma += broj % 10;
-		broj /= 10;
-	}
-	return Suma;
-}
-void ispisXY(int X, int Y) {
-	int obrni;
-	int sumac;
-	for (int i = X; i <= Y; i++)
+
+int obrni(int broj)
+{
+	int novi = 0;
+	while (broj > 0)
 	{
-		obrni = Obrnibroj(i);
-		sumac = sumaCifri(i);
-		if (obrni % sumac == 0) {
-			cout << i << " ";
-		}
-		/*else {
-			i++;
-		}*/
+		int temp = broj % 10;
+		novi = novi * 10 + temp;
+		broj /= 10;
+	}return novi;
+}
+
+int suma(int broj)
+{
+	int suma = 0;
+	while (broj > 0)
+	{
+		int temp = broj % 10;
+		suma += temp;
+		broj /= 10;
+	}return suma;
+}
+
+int main()
+{
+	int x = unos();
+	int y = unos();
+	
+	if (x > y)
+	{
+		int temp = x;
+		x = y;
+		y = temp;
 	}
+	cout << "Brojevi od " << x << " do " << y << " koji su obrnuti djeljivi sa sumom svojih cifara su : " << endl;
+	for (int i = x; i <= y; i++)
+	{
+		if (obrni(i) % suma(i) == 0)
+			cout << i << endl;
+	}
+	system("pause>0");
+	return 0;
 }
