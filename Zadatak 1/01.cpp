edbc1275f1cd619b9dcd050e99f5f1f965faf771
Muhamed Redžić <92086961+RedzicMuhamed@users.noreply.github.com>
2021-10-07 12:07:14 +0200

    Update 01.cpp

--- Zadatak 1/01.cpp
+++ Zadatak 1/01.cpp
@@ -1,43 +1,51 @@
 
 #include <iostream>
-#include <cmath>
 using namespace std;
-	float sinx(float);
-	float cosx(float);
-	int main() {
-	float broj = 0.0f;
-	char izbor;
-	cout << "Unesite neki realan broj. " << endl;
-	cin >> broj;
-	do{
-		cout << "Odaberite funkciju. s za sinus i c za cosinus." << endl;
-		cin >> izbor;
-		if (izbor == 's')
-		cout << sinx(broj);
-		else if (izbor == 'c')
-		cout << cosx(broj);
-		else cout << "Niste unijeli odgovarajuci unos. Molimo unesite ispravnu funkciju." << endl;
-	} while (izbor !='s' && izbor!='c');
-		system("pause>null");
-		return 0;
-	}
-	float sinx(float broj) {
-	double suma=1.0f;
-	float const PI = 3.14159265;
-	for (int i = 1; i <= broj; i++)
-	{
-		suma += i / (sin(PI / 180 *( broj / i)) + broj);
-	}
-	cout << "Rezultat unesene funkcije je: ";
-	return suma;
-	}
-	float cosx(float broj) {
-	double suma = 1.0f;
-	float const PI = 3.14159265;
-	for (int i=1 ; i <= broj; i++)
-	{
-		suma += i / (cos(PI / 180 *(broj / i)) + broj);
-	}
-	cout << "Rezulatat unesene funkcije je: ";
-	return suma;
+
+float unosx()
+{
+	float x;
+	cout << "Unesite realan broj x" << endl;
+	cin >> x;
+	return x;
 }
+
+char unos_znaka()
+{
+	char izbor;
+	do
+	{
+		cout << "Unesite znak s ili c" << endl;
+		cin >> izbor;
+
+		if (izbor != 's' && izbor != 'c')
+			cout << "Unijeli ste nevazeci znak" << endl;
+
+	} while (izbor != 's' && izbor != 'c');
+	return izbor;
+}
+
+float proracun(float x, char izbor)
+{
+	float rezultat = 1.0;
+	if (izbor == 's')
+	{
+		for (int i = 1; i <= x; i++)
+			rezultat += i / (sin(x / i) + x);
+	}
+	else
+	{
+		for (int i = 1; i <= x; i++)
+			rezultat += i / (cos(x / i) + x);
+	}
+	return rezultat;
+}
+
+int main()
+{
+	float x = unosx();
+	char izbor = unos_znaka();
+	
+	cout << "Proracun datog izraza je " << proracun(x, izbor) << endl;
+}
+
