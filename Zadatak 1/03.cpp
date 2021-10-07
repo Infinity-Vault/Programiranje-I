c12d54690bb5b70c17e1513c089f1fa132dcf097
Muhamed Redžić <92086961+RedzicMuhamed@users.noreply.github.com>
2021-10-07 15:56:18 +0200

    Update 03.cpp

--- Zadatak 1/03.cpp
+++ Zadatak 1/03.cpp
@@ -1,41 +1,30 @@
 #include <iostream>
-
 using namespace std;
 
-int unos();
-
-int main() {
-	
-	int sastavljeniBr = 0;
-	int cifra = 0;
-	
-	do {
-		
-		cifra = unos();
-		
-		if (cifra < 0)//Dodatna provjera da se ne bi sracunao neg broj, jer se uslov gleda na kraju zbog do while;
-			break;
-		
-		if (cifra > 9)
-			cout << "Cifra nije validna" << endl;
-		
-		else {
-			sastavljeniBr = sastavljeniBr * 10 + cifra;
-		}
-		
-	} while (cifra >= 0);
-	
-	cout << "Od unesenih cifri je sastavljen broj: " << sastavljeniBr << endl;
-
-	cin.get();
-	return 0;
-}
-int unos() {
-	
-	int broj;
-	
-	cout << "Unesite broj: " << endl;
+void unos(int& broj)
+{
+	cout << "Upisi cifru " << endl;
 	cin >> broj;
-	
-	return broj;
+}
+
+int main()
+{
+	int broj = 0, novi = 0;
+	do
+	{
+		unos(broj);
+		if (broj <= 9 && broj >= 0)
+		{
+			novi = novi * 10 + broj;
+		}
+
+		if (broj > 9)
+		{
+			cout << "Cifra nije validna " << endl;
+		}
+	} while (broj >= 0);
+
+	cout << "Sastavljen broj je " << novi << endl;
+	system("pause>0");
+	return 0;
 }
