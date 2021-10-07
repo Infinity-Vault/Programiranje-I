f4dacf9a351b9a404bc363538be9785ff275f2ec
Muhamed Redžić <92086961+RedzicMuhamed@users.noreply.github.com>
2021-10-07 16:30:26 +0200

    Update 01.cpp

--- Zadatak 3/01.cpp
+++ Zadatak 3/01.cpp
@@ -1,88 +1,51 @@
-﻿#include <iostream>
-
+#include <iostream>
 using namespace std;
 
-void napuniNiz(int*, int, int brojac = 0); // int brojac = 0 je default parametar, važi za svaki poziv funkcije tako da se ne mora proslijeđivati
-void ispisiNiz(int*, int, int brojac = 0);
-
-long long int suma(int*, int, int brojac = 0); // Koristimo long long int zbog mogućnosti velikog rezultata
-
-int main() {
-	
-	int velicina;
-	
-	do {
-		
-		cout << "Unesite velicinu niza: " << endl;
-		cin >> velicina;
-		
-	} while (velicina < 2);
-	
-	int* niz = new int[velicina] {};//{} - inicijalizacija 0;
-	
-	napuniNiz(niz, velicina);
-	ispisiNiz(niz, velicina);
-	
-	cout << endl;
-	
-	cout << "Suma svih elemenata niza je: " << suma(niz, velicina);//Treci parametar je default;
-
-	// Dealokacija:
-	delete[] niz;
-	niz = nullptr;
-	
-	cin.get();
-	return 0;
- }
-
-void napuniNiz(int* niz, int velicina, int brojac) {
-
-	// Kada brojac dođe do velicine niza, tada se rekurzija prekida jer ne želimo pisati van njegovog opsega
-	if (brojac == velicina)
+void rekurzija(int* niz, int vel, int brojac = 0)
+{
+	if (brojac == vel)
 		return;
-
-	// Sve dok je brojac manji od velicine rekurzija se izvrsava
-	if (brojac < velicina) { 
-
-		// Postavljamo prva dva elementa niza kao 1 i 3 zbog zahtjeva zadatka
-		if (brojac == 0)
-			*(niz + brojac) = 1;
-		
-		if (brojac == 1)
-			*(niz + brojac) = 3;
-
-		// Kada postavimo prva dva elementa onda nalazimo sljedece
-		if (brojac > 1) {
-
-			// Umnozak dva prethodna pohranjujemo u temp varijablu
-			int temp = *(niz + brojac - 1) * *(niz + brojac - 2);
-			
-			if (temp < 0) // Prekidamo rekurziju ako se desi overflow na int temp (overflow na signed int promijeni vrijednost u negativni broj)
-				return;
-			
-			*(niz + brojac) = temp; 
-		}
-
-		// Nakon svakog postavljanja novog elementa nastavljamo rekurziju novim pozivom gdje se brojac inkrementuje za jedan (da bi dosli do sljedeceg elementa)
-		napuniNiz(niz, velicina, brojac + 1);
+	if (brojac == 0)
+		*(niz + brojac) = 1;
+	else if (brojac == 1)
+		*(niz + brojac) = 3;
+	else
+	{
+		if (*(niz + brojac - 2) * *(niz + brojac - 1) < 0)
+			return;
+		else
+			*(niz + brojac) = *(niz + brojac - 1) * *(niz + brojac - 2);
 	}
+	rekurzija(niz, vel, brojac + 1);
 }
 
-void ispisiNiz(int* niz, int velicina, int brojac) {
-
-	if (brojac == velicina)
-		return;
-	
-	cout << *(niz + brojac) << " ";
-	
-	ispisiNiz(niz, velicina, brojac + 1);
+void ispis(int* niz, int vel)
+{
+	for (int i = 0; i < vel; i++)
+		cout << *(niz + i) << endl;
 }
 
-long long int suma(int* niz, int velicina, int brojac) {
-	
-	if (brojac == velicina)
-		return 0;
+int suma(int* niz, int vel)
+{
+	if (vel == 0)
+		return *(niz + vel);
+	return *(niz + vel) + suma(niz, vel - 1);
+}
 
-	// Svakim rekurzivnim pozivom sumiramo trenutni element niza a inkrementujemo brojac za 1 kako bi dosli do sljedećeg elementa
-	return *(niz + brojac) + suma(niz, velicina, brojac + 1);
+int main()
+{
+	int vel;
+	do
+	{
+		cout << "Unesite velicinu niza " << endl;
+		cin >> vel;
+	} while (vel < 2);
+	int* niz = new int[vel]; 
+	rekurzija(niz, vel);
+	ispis(niz, vel);
+	cout << "Suma clanova niza je " << suma(niz, vel - 1) << endl;
+	delete[]niz;
+	niz = nullptr;
+	system("pause>0");
+	return 0;
 }
